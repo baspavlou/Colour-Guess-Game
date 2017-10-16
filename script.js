5c7a32ebb5b8d75f2e8890756b1fdbb8bdaf9c71
Basilis Pavlou <baspavlou@hotmail.com>
2017-10-16 12:07:43 +0300

    added rgb to hex converter

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ var newgame=document.getElementById('newgame');
 var easymode=false;
 
 for(var i=0;i<cells.length;i++){
-	cells[i].style.background='rgb('+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+')';
+	var r=Math.floor((Math.random()*256)+1);
+	var g=Math.floor((Math.random()*256)+1);
+	var b=Math.floor((Math.random()*256)+1);
+	cells[i].style.background='rgb('+r+','+g+','+b+')';
 	var titlecolor=cells[Math.floor(Math.random()*7+1)].style.background;
 }
 
 
+
 document.getElementsByTagName('h1')[0].innerHTML=titlecolor;
+var regexR = /\((.*?)\,/;
+var regexG = /\,(.*?)\,/;
+var regexB = /\,(.*?)\)/;
+r1 = regexR.exec(titlecolor)[1];
+g1 = regexG.exec(titlecolor)[1];
+b1 = regexB.exec(titlecolor)[1];
+b2=b1.replace(g1,'');
+b3=b2.slice(2,b2.length);
+document.getElementsByTagName('h1')[1].innerHTML=rgbToHex(Number(r1), Number(g1),Number(b3)) ;
 
 for(var i=0;i<cells.length;i++){
 	if(gameOver===false){
@@ -50,19 +63,32 @@ newgame.addEventListener('click',function(){
 	if(easymode==false){
 				
 				for(var i=0;i<cells.length;i++){
-					cells[i].style.background='rgb('+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+')';
+					var r=Math.floor((Math.random()*256)+1);
+					var g=Math.floor((Math.random()*256)+1);
+					var b=Math.floor((Math.random()*256)+1);
+					cells[i].style.background='rgb('+r+','+g+','+b+')';
 					titlecolor=cells[Math.floor(Math.random()*7+1)].style.background;
 				}
 			}
 			else{
 				for(var i=0;i<3;i++){
-					cells[i].style.background='rgb('+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+')';
+					var r=Math.floor((Math.random()*256)+1);
+					var g=Math.floor((Math.random()*256)+1);
+					var b=Math.floor((Math.random()*256)+1);
+					cells[i].style.background='rgb('+r+','+g+','+b+')';
 					titlecolor=cells[Math.floor(Math.random()*2+1)].style.background;
 				}
 			}
 	document.getElementById('score').innerHTML="Which one ?";
 	gameOver=false;
 	document.getElementsByTagName('h1')[0].innerHTML=titlecolor;
+	r1 = regexR.exec(titlecolor)[1];
+g1 = regexG.exec(titlecolor)[1];
+b1 = regexB.exec(titlecolor)[1];
+b2=b1.replace(g1,'');
+b3=b2.slice(2,b2.length);
+document.getElementsByTagName('h1')[1].innerHTML=rgbToHex(Number(r1), Number(g1),Number(b3)) ;
+	
 });
 
 document.getElementById('modeeasy').addEventListener('click',function(){
@@ -71,22 +97,52 @@ document.getElementById('modeeasy').addEventListener('click',function(){
 		easymode=true;
 	}
 	for(var i=0;i<3;i++){
-		cells[i].style.background='rgb('+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+')';
+		var r=Math.floor((Math.random()*256)+1);
+		var g=Math.floor((Math.random()*256)+1);
+		var b=Math.floor((Math.random()*256)+1);
+		cells[i].style.background='rgb('+r+','+g+','+b+')';
 		titlecolor=cells[Math.floor(Math.random()*2+1)].style.background;
 	}
 	document.getElementById('score').innerHTML="Which one ?";
 	gameOver=false;
 	document.getElementsByTagName('h1')[0].innerHTML=titlecolor;
+	r1 = regexR.exec(titlecolor)[1];
+g1 = regexG.exec(titlecolor)[1];
+b1 = regexB.exec(titlecolor)[1];
+b2=b1.replace(g1,'');
+b3=b2.slice(2,b2.length);
+document.getElementsByTagName('h1')[1].innerHTML=rgbToHex(Number(r1), Number(g1),Number(b3)) ;
+	
 });
 
 document.getElementById('modehard').addEventListener('click',function(){
 
 	easymode=false;
 	for(var i=0;i<cells.length;i++){
-					cells[i].style.background='rgb('+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+','+Math.floor((Math.random()*256)+1)+')';
+				var r=Math.floor((Math.random()*256)+1);
+				var g=Math.floor((Math.random()*256)+1);
+				var b=Math.floor((Math.random()*256)+1);
+					cells[i].style.background='rgb('+r+','+g+','+b+')';
 					titlecolor=cells[Math.floor(Math.random()*7+1)].style.background;
 				}
 	document.getElementById('score').innerHTML="Which one ?";
 	gameOver=false;
 	document.getElementsByTagName('h1')[0].innerHTML=titlecolor;
+	r1 = regexR.exec(titlecolor)[1];
+g1 = regexG.exec(titlecolor)[1];
+b1 = regexB.exec(titlecolor)[1];
+b2=b1.replace(g1,'');
+b3=b2.slice(2,b2.length);
+document.getElementsByTagName('h1')[1].innerHTML=rgbToHex(Number(r1), Number(g1),Number(b3)) ;
+	
 });
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
